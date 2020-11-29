d6f5928493c975163bc6c36605e48de122537d2f
KarenIriguchi <ladyren666@gmail.com>
2020-11-30 01:10:57 +0900

    スコアの実装
    スコアの実装

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject gameOverText;
     [SerializeField] GameObject gameClearText;
+    [SerializeField] Text ScoreText;
+
+    //アイテム取得時のスコア加算式
+
+    const int MAX_SCORE = 9999;
+    int score = 0;
+
+    //開始時のスコアを0で固定
+    private void Start()
+    {
+        ScoreText.text = score.ToString();
+    }
+
+    public void AddScore(int val)
+    {
+        score += val;
+        if ( score > MAX_SCORE )
+        {
+            score = MAX_SCORE;
+        }
+
+        //ScoreTextに反映するがint型になっているのでString型にする必要がある
+        ScoreText.text = score.ToString();
+    }
 
     //ゲームオブジェクトを取得して表示
     public void GameOver()
--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -2,9 +2,18 @@
 
 public class ItemManager : MonoBehaviour
 {
+    //GameManagerを呼ぶ
+    GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
     //アイテムは取得したら破壊する
     public void GetItem()
     {
+        gameManager.AddScore(100);
         Destroy(this.gameObject);
     }
 }
